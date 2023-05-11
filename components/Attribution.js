a9debd7dae25dbeb05abf9a5350f4150bc9c88f7
Yuniski <yuniski24@gmail.com>
2023-05-11 17:07:27 -0300

    components js

---
+++ components/Attribution.js
@@ -0,0 +1,8 @@
+export default function Attribution () {
+  return `
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/Yuniski/interactive-rating-component" target="_blank" rel="noopener">Niki Bear 🐻🚀</a>.
+    </div>
+  `;
+}
--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+import Attribution from './components/Attribution';
+import Rating from './components/Rating';
+import Thanks from './components/Thanks';
+
+const app = document.getElementById('app');
+
+app.innerHTML += Rating;
+app.innerHTML += Attribution;
+app.innerHTML += Thanks;
